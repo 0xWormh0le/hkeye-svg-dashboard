ef5f6a2d2700a6e1f2aa334c59657c0527042b02
UnivDev <univdev99@gmail.com>
2020-10-06 23:44:01 -0400

    Build footer component

--- src/components/SkewedRect/index.js
+++ src/components/SkewedRect/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './style.scss';
 
-export default function SkewedRect({ compName, posX, posY, shapeColor, title, variant, textWidth}) {
+export default function SkewedRect({ compName, posX, posY, title, variant, textWidth, textDirection}) {
   return (
 
     <div id={ compName } className='SkewedRect' style={{ left: posX, top: posY }}>
-      <div className={'parallelogram '+ variant } style={{ backgroundColor: shapeColor  }}>
-        <p className='skewedText' style={{ width: textWidth }}>
+      <div className={'parallelogram '+ variant }>
+        <p className={'skewedText ' + textDirection} style={{ width: textWidth }}>
           { title }
         </p>
       </div>
