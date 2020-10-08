0eeeedb13be81e96ca79c9595055e4ee4fe9d38b
UnivDev <univdev99@gmail.com>
2020-10-08 13:31:10 -0400

    Merge bodySide

--- src/components/SkewedRect/index.js
+++ src/components/SkewedRect/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import './style.scss';
 
-export default function SkewedRect({ compName, posX, posY, title, variant, textWidth, textDirection}) {
+export default function SkewedRect({ compName, style, title, variant, textWidth, textDirection}) {
   return (
 
-    <div id={ compName } className='SkewedRect' style={{ left: posX, top: posY }}>
+    <div id={ compName } className='SkewedRect' style={style}>
       <div className={'parallelogram '+ variant }>
         <p className={'skewedText ' + textDirection} style={{ width: textWidth }}>
           { title }
